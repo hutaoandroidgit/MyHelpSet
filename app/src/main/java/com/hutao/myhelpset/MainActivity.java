7e7cdebdda44447ea48caf0a451c3b8de8fe15b5
hutao <hutaoandroid@163.com>
2021-08-30 13:57:27 +0800

    Initial commit

---
+++ app/src/main/java/com/hutao/myhelpset/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hutao.myhelpset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
